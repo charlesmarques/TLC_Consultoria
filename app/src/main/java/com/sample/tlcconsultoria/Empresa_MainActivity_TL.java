dd2442abfdde057c856cf2528d9c3a3d581bb9eb
Charles Marques <charlesmarquesdf@hotmail.com>
2018-10-30 17:16:50 -0300

    commit inicial

---
+++ app/src/main/java/com/sample/tlcconsultoria/Empresa_MainActivity_TL.java
@@ -0,0 +1,14 @@
+package com.sample.tlcconsultoria;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Empresa_MainActivity_TL extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa__main__tl);
+    }
+}
---
+++ app/src/main/java/com/sample/tlcconsultoria/Princiapl_MainActivity_TL.java
@@ -0,0 +1,26 @@
+package com.sample.tlcconsultoria;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Princiapl_MainActivity_TL extends Activity {
+private ImageView obtn_Empresa;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_princiapl__main__tl);
+
+        obtn_Empresa = (ImageView) findViewById(R.id.btn_Empresa);
+
+        obtn_Empresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Princiapl_MainActivity_TL.this ,Empresa_MainActivity_TL.class));
+            }
+        });
+    }
+}
